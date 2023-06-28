dcaae9a0aa7d122a8ddc90fae7c5bd1a292e3075
Egor Muzychkin <e.myzichkin@rit.va>
2023-06-28 11:22:46 +0300

    add basic app styles

--- src/styles/app.css.ts
+++ src/styles/app.css.ts
@@ -7,8 +7,27 @@ export const app = style({
     backgroundColor: colorScheme.background.normal,
     color: colorScheme.text.normal,
     transition: "0.2s",
-    // width: "100vw",
-    height: "100vh"
+    width: "100%",
+    flex: 1,
+    height: "100vh",
+})
+
+globalStyle("body", {
+    fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+    lineHeight: 1.5,
+    fontWeight: 400,
+
+    fontSynthesis: "none",
+    textRendering: "optimizeLegibility",
+    WebkitFontSmoothing: "antialiased",
+    MozOsxFontSmoothing: "grayscale",
+    WebkitTextSizeAdjust: "100%",
+
+    display: "flex",
+})
+
+globalStyle("#root", {
+    flex: 1
 })
 
 globalStyle("*", {
