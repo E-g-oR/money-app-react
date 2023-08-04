5466006b2d8c7c1c1a7078d94fb81f35bc7d3d34
Egor Muzychkin <e.myzichkin@rit.va>
2023-08-04 09:13:27 +0300

    fix: protected layouts

--- src/pages/auth/auth-layout/AuthLayout.tsx
+++ src/pages/auth/auth-layout/AuthLayout.tsx
@@ -1,29 +1,24 @@
 import * as styles from "./auth-layout.css"
-import {FC, useEffect} from "react";
+import {FC} from "react";
 import {Card, Stack} from "@components";
-import {Outlet, useNavigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {Navigate, Outlet} from "react-router-dom";
 import {getAccessToken} from "@store/auth/auth.selector.ts";
+import useAuthStore from "@store/auth/auth.slice.ts";
 import {ROUTES} from "@utils/router.ts";
 
 
 const AuthLayout: FC = () => {
-    const navigate = useNavigate()
-    const accessToken = useSelector(getAccessToken)
+    const accessToken = useAuthStore(getAccessToken)
 
-    useEffect(() => {
-        if (accessToken) {
-            navigate(ROUTES.main.path)
-        }
-    }, [accessToken, navigate])
-
-    return <div className={styles.layout}>
-        <Card padding={"m"}>
-            <Stack vertical>
-                <Outlet/>
-            </Stack>
-        </Card>
-    </div>
+    return !accessToken
+        ? <div className={styles.layout}>
+            <Card padding={"m"}>
+                <Stack vertical>
+                    <Outlet/>
+                </Stack>
+            </Card>
+        </div>
+        : <Navigate to={ROUTES.main.path}/>
 }
 
 export default AuthLayout
