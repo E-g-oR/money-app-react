8688d44d70d685f98ff1666b1d935e27f90918b3
Egor Muzychkin <e.myzichkin@rit.va>
2023-08-16 12:08:52 +0300

    redactor: add form validation creating dept

--- src/pages/depths/AddDepthModal.tsx
+++ src/pages/depths/AddDepthModal.tsx
@@ -1,84 +1,99 @@
 import {Button, IconButton, Input, Modal, Stack} from "@/components";
 import {FC, useCallback, useState} from "react";
 import {useTranslation} from "@utils/hooks.tsx";
+import {Controller, SubmitHandler, useForm} from "react-hook-form";
 import Api from "@api";
-import {CreateDepthDto} from "@/types/API/data-contracts.ts";
+
+interface CreateDepthModal {
+    deptTitle: string
+    deptDescription: string,
+    deptValue: string,
+    deptCoveredValue: string
+}
 
 const AddDepthModal: FC = () => {
     const t = useTranslation()
-    // const [createDepth, {isLoading, isSuccess}] = useCreateDepthMutation()
+
+    const {handleSubmit, reset, control} = useForm<CreateDepthModal>({
+        defaultValues: {
+            deptTitle: "",
+            deptDescription: "",
+            deptValue: "",
+            deptCoveredValue: "",
+        }
+    })
 
     const [isOpen, setIsOpen] = useState(false)
-    const [depthName, setDepthName] = useState("")
-    const [depthValue, setDepthValue] = useState("")
-    const [depthValueCovered, setDepthValueCovered] = useState("")
-    const [depthDescription, setDepthDescription] = useState("")
 
-    const onClose = useCallback(() => {
-        setDepthName("")
-        setDepthValue("")
-        setDepthValueCovered("")
-        setDepthDescription("")
-        setIsOpen(false)
-    }, [setDepthName, setDepthValue, setDepthDescription, setDepthValueCovered, setIsOpen])
-
-    const createDepth = useCallback((body: CreateDepthDto) => {
-        Api.createDepth(body).then(() => {
-            onClose()
+    const onSubmit: SubmitHandler<CreateDepthModal> = useCallback((data) => {
+        Api.createDepth({
+            value: parseFloat(data.deptValue),
+            title: data.deptTitle,
+            description: data.deptDescription,
+            valueCovered: data.deptCoveredValue.length > 0 ? parseFloat(data.deptCoveredValue) : 0,
+            deadline: new Date().toISOString()
+        }).then(() => {
+            setIsOpen(false)
+            reset()
         })
-    }, [onClose])
+    }, [setIsOpen, reset])
 
     return <>
         <Modal
-            onClose={onClose}
+            onClose={() => setIsOpen(false)}
             isOpen={isOpen}
             title={t.depts.createDept}
         >
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                createDepth({
-                    value: Number(depthValue),
-                    title: depthName,
-                    description: depthDescription,
-                    valueCovered: Number(depthValueCovered),
-                    deadline: new Date().toISOString()
-                })
-            }}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <Stack vertical spacing={"s"}>
-                    <Input
-                        placeholder={t.common.title}
-                        value={depthName}
-                        onChange={setDepthName}
+                    <Controller control={control}
+                                rules={{
+                                    required: true
+                                }}
+                                render={({field, fieldState}) => <Input
+                                    placeholder={t.common.title}
+                                    isError={fieldState.invalid}
+                                    {...field}
+                                />}
+                                name={"deptTitle"}
                     />
-                    <Input
-                        placeholder={t.common.value}
-                        type={"number"}
-                        value={depthValue}
-                        onChange={setDepthValue}
+                    <Controller control={control}
+                                render={({field}) => <Input
+                                    placeholder={t.common.description}
+                                    {...field}
+                                />}
+                                name={"deptDescription"}
                     />
-                    <Input
-                        placeholder={t.depts.coveredValue}
-                        type={"number"}
-                        value={depthValueCovered}
-                        onChange={setDepthValueCovered}
-                    />
-                    <Input
-                        placeholder={t.common.description}
-                        value={depthDescription}
-                        onChange={setDepthDescription}
-                    />
+                    <Stack spacing={"m"}>
+                        <Controller control={control}
+                                    rules={{
+                                        required: true,
+                                        min: 0.01
+                                    }}
+                                    render={({field}) => <Input
+                                        placeholder={t.common.value}
+                                        type={"number"}
+                                        fullWidth
+                                        {...field}
+                                    />}
+                                    name={"deptValue"}
+                        />
+                        <Controller control={control}
+                                    render={({field}) => <Input
+                                        placeholder={t.depts.coveredValue}
+                                        type={"number"}
+                                        fullWidth
+                                        {...field}
+                                    />}
+                                    name={"deptCoveredValue"}
+                        />
+                    </Stack>
+
+
                     <Button
                         size={"m"}
                         type={"submit"}
-                        onClick={() => {
-                            createDepth({
-                                value: Number(depthValue),
-                                title: depthName,
-                                description: depthDescription,
-                                valueCovered: Number(depthValueCovered),
-                                deadline: new Date().toISOString()
-                            })
-                        }}
+                        onClick={handleSubmit(onSubmit)}
                     >{t.actions.create}</Button>
                 </Stack>
             </form>
