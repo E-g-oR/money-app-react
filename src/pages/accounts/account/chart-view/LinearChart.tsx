05feae2dbba61095a808a5fad98e04672ac5c467
Egor Muzychkin <e.myzichkin@rit.va>
2023-08-23 11:19:46 +0300

    chart styles

--- src/pages/accounts/account/chart-view/LinearChart.tsx
+++ src/pages/accounts/account/chart-view/LinearChart.tsx
@@ -4,7 +4,7 @@ import {Line, LinePath} from "@visx/shape";
 import {GridColumns, GridRows} from "@visx/grid";
 import {Axis, AxisLeft, Orientation} from '@visx/axis';
 import {WithTooltipProvidedProps} from "@visx/tooltip/lib/enhancers/withTooltip";
-import {defaultStyles, TooltipWithBounds, withTooltip} from "@visx/tooltip";
+import {TooltipWithBounds, withTooltip} from "@visx/tooltip";
 import {ChartPoint, getProcessedData, getX, getY} from "@utils/charts.ts";
 import {
     ChartMargin,
@@ -16,14 +16,11 @@ import {
 } from "@utils/hooks.tsx";
 import {ChartDataDto} from "@/types/API/data-contracts.ts";
 import {Stack, Typography} from "@components";
-
-
-const tooltipStyles = {
-    ...defaultStyles,
-    borderWidth: 1,
-    borderStyle: "solid",
-};
-
+import {clsx} from "@utils/etc.ts";
+import colors from "tailwindcss/colors";
+import useSettingsStore from "@store/settings/settings.slice.ts";
+import {getColorScheme} from "@store/settings/settings.selector.ts";
+import {useMemo} from "react";
 
 interface Props {
     size: ChartSize,
@@ -62,7 +59,10 @@ export default withTooltip<Props, TooltipData_>(
             innerHeight,
         } = useLinearChart(data, size, margin)
 
+        const colorScheme = useSettingsStore(getColorScheme)
 
+        const axisStroke = useMemo(() => colorScheme === "light" ? colors.zinc["700"] : colors.zinc["300"], [colorScheme])
+        const tooltipPointColor = useMemo(() => colorScheme === "light" ? colors.indigo[700] : colors.indigo[400], [colorScheme])
         return <div className={"chart"}>
             <svg width={size.width} height={size.height}>
                 <GridRows
@@ -70,9 +70,8 @@ export default withTooltip<Props, TooltipData_>(
                     left={margin.left}
                     scale={yScale}
                     width={innerWidth}
-                    stroke={"#e0acf6"}
-                    strokeWidth={0.5}
-                    strokeOpacity={0.3}
+                    stroke={axisStroke}
+                    strokeOpacity={0.2}
                 />
                 <GridColumns
                     top={margin.top}
@@ -80,9 +79,8 @@ export default withTooltip<Props, TooltipData_>(
                     scale={axisTimeScale}
                     width={innerWidth}
                     height={innerHeight}
-                    stroke={"#ff0000"}
-                    strokeOpacity={0.5}
-                    strokeWidth={0.3}
+                    stroke={axisStroke}
+                    strokeOpacity={0.2}
                 />
                 {data.chartLines.map(({lineKey, lineData}) => {
                     const processedData = getProcessedData(lineData ?? [])
@@ -94,21 +92,19 @@ export default withTooltip<Props, TooltipData_>(
                         {processedData?.map(point => {
                                 const xPosition = xScale(getX(point)),
                                     yPosition = yScale(getY(point));
-
                                 return <Group key={getX(point).toISOString()}>
                                     <circle
                                         r={3}
                                         cx={xPosition}
                                         cy={yPosition}
-                                        stroke={"#ff0000"}
-                                        fill={"#ff0000"}
+                                        className={lineKey === "expenses"
+                                            ? "fill-error-500 stroke-error-500"
+                                            : "fill-success-500 stroke-success-500"}
                                     />
                                     <circle
-                                        r={30}
+                                        r={20}
                                         cx={xPosition}
                                         cy={yPosition}
-                                        stroke={"#ff0000"}
-                                        fill={"#ff0000"}
                                         onMouseMove={() => {
                                             showTooltip({
                                                 tooltipData: {
@@ -119,8 +115,13 @@ export default withTooltip<Props, TooltipData_>(
                                                 tooltipLeft: xPosition
                                             })
                                         }}
-                                        fillOpacity={0}
-                                        strokeOpacity={0}
+                                        className={
+                                            clsx(
+                                                "transition",
+                                                lineKey === "expenses"
+                                                    ? "fill-error-500/5 hover:fill-error-500/30"
+                                                    : "fill-success-500/5 hover:fill-success-500/30"
+                                            )}
                                         onMouseLeave={hideTooltip}
                                     />
                                 </Group>
@@ -132,7 +133,9 @@ export default withTooltip<Props, TooltipData_>(
                             data={processedData}
                             x={d => xScale(getX(d)) ?? 0}
                             y={d => yScale(getY(d)) ?? 0}
-                            stroke={"#ff0000"}
+                            className={lineKey === "expenses"
+                                ? "stroke-error-600 dark:stroke-error-400"
+                                : "stroke-success-600 dark:stroke-success-400"}
                             strokeWidth={2}
                             strokeOpacity={0.6}
                             shapeRendering="geometricPrecision"
@@ -142,11 +145,11 @@ export default withTooltip<Props, TooltipData_>(
                 <AxisLeft
                     scale={yScale}
                     left={margin.left}
-                    stroke={"#000000"}
-                    tickStroke={"#000000"}
+                    stroke={axisStroke}
+                    tickStroke={axisStroke}
                     top={(margin.top ?? 0)}
                     tickLabelProps={{
-                        fill: "#000000",
+                        fill: axisStroke,
                     }}
                 />
                 <Axis
@@ -155,11 +158,11 @@ export default withTooltip<Props, TooltipData_>(
                     orientation={Orientation.bottom}
                     tickValues={axisTimeValues}
                     tickFormat={view === "month" ? t.formatDate.chartAxisInMonthView : t.formatDate.chartAxisInYearView}
-                    stroke={"#000000"}
-                    tickStroke={"#000000"}
+                    stroke={axisStroke}
+                    tickStroke={axisStroke}
                     top={size.height - (margin.top ?? 0)}
                     tickLabelProps={{
-                        fill: "#000000"
+                        fill: axisStroke
                     }}
                 />
                 {
@@ -168,7 +171,7 @@ export default withTooltip<Props, TooltipData_>(
                             <Line
                                 from={{x: tooltipLeft + margin?.left, y: margin.top}}
                                 to={{x: tooltipLeft + margin?.left, y: innerHeight + margin.top}}
-                                stroke={"#0000ff"}
+                                stroke={tooltipPointColor}
                                 strokeWidth={2}
                                 pointerEvents="none"
                                 strokeDasharray="10,10"
@@ -176,7 +179,7 @@ export default withTooltip<Props, TooltipData_>(
                             <Line
                                 from={{x: margin?.left, y: tooltipTop + margin?.top}}
                                 to={{x: innerWidth + margin?.left, y: tooltipTop + margin?.top}}
-                                stroke={"#0000ff"}
+                                stroke={tooltipPointColor}
                                 strokeWidth={2}
                                 pointerEvents="none"
                                 strokeDasharray="10,10"
@@ -185,7 +188,7 @@ export default withTooltip<Props, TooltipData_>(
                                 cx={tooltipLeft + margin?.left}
                                 cy={tooltipTop + margin?.top}
                                 r={4}
-                                fill={"#0000ff"}
+                                fill={tooltipPointColor}
                                 stroke="white"
                                 strokeWidth={2}
                                 pointerEvents="none"
@@ -193,7 +196,6 @@ export default withTooltip<Props, TooltipData_>(
                         </g>
                     )
                 }
-
             </svg>
             {tooltipOpen && tooltipData && (
                 <div>
@@ -201,7 +203,7 @@ export default withTooltip<Props, TooltipData_>(
                         key={Math.random()}
                         top={tooltipTop - 30}
                         left={tooltipLeft + 35}
-                        style={tooltipStyles}
+                        className={"bg-background-900"}
                     >
                         <Stack spacing={"s"} vertical>
                             <Typography>{view === "month" ? t.formatDate.chartDateInMonthView(tooltipData?.item.date) : t.formatDate.chartDateInYearView(tooltipData.item.date)}</Typography>
@@ -209,7 +211,6 @@ export default withTooltip<Props, TooltipData_>(
                                 as={"i"}>{tooltipData?.lineKey}</Typography>: {tooltipData?.item.value} </Typography>}
                         </Stack>
                     </TooltipWithBounds>
-
                 </div>
             )}
         </div>
